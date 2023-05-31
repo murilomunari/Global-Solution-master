d491d558b5637169dc7687c09633d7ba7d3d4da7
murilomunari <munarimurilo441@gmail.com>
2023-05-31 11:09:04 -0300

    conversar com o professor

---
+++ src/br/com/pessoa/model/Agricultor.java
@@ -0,0 +1,14 @@
+package br.com.pessoa.model;
+
+import br.com.sistema.model.Sistema;
+
+public class Agricultor {
+
+    private String plantcao;
+
+    private String quantidade;
+
+    private Sistema agricultor;
+
+
+}
--- src/br/com/sistema/model/Cadastramento.java
+++ src/br/com/sistema/model/Cadastramento.java
@@ -1,4 +1,65 @@
 package br.com.sistema.model;
 
-public class Cadastramento {
+public class Cadastramento extends Sistema{
+
+    private String nome;
+
+    private String CPF;
+
+    private String CNPJ;
+
+    private String localizacao;
+
+    public Cadastramento() {
+    }
+
+    public Cadastramento(String nome, String CPF, String CNPJ, String localizacao) {
+        this.nome = nome;
+        this.CPF = CPF;
+        this.CNPJ = CNPJ;
+        this.localizacao = localizacao;
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    public String getCNPJ() {
+        return CNPJ;
+    }
+
+    public void setCNPJ(String CNPJ) {
+        this.CNPJ = CNPJ;
+    }
+
+    public String getLocalizacao() {
+        return localizacao;
+    }
+
+    public void setLocalizacao(String localizacao) {
+        this.localizacao = localizacao;
+    }
+
+    @Override
+    public String toString() {
+        return "Cadastramento{" +
+                "nome='" + nome + '\'' +
+                ", CPF='" + CPF + '\'' +
+                ", CNPJ='" + CNPJ + '\'' +
+                ", localizacao='" + localizacao + '\'' +
+                '}';
+    }
 }
